c1b06b9099ec4cf5641c5a25e43cfe6bcf7a22d4
Bradley Pagano <bpagano5243@gmail.com>
2019-10-23 10:06:44 -0400

    it's alive

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,12 +18,18 @@ $(document).ready(function(){
     $("#choice4").val(randomNum4);
 
     function reset(){
-        let computerNumber = Math.floor(Math.random()*(120-19)+19);
+        computerNumber = Math.floor(Math.random()*(120-19)+19);
+        $(".randomNum").text(computerNumber);
         playerNumber = 0;
-        let randomNum1 = Math.floor(Math.random()*(12-1)+1);
-        let randomNum2 = Math.floor(Math.random()*(12-1)+1);
-        let randomNum3 = Math.floor(Math.random()*(12-1)+1);
-        let randomNum4 = Math.floor(Math.random()*(12-1)+1);
+        $(".yourNum").text(playerNumber);
+        randomNum1 = Math.floor(Math.random()*(12-1)+1);
+        $("#choice1").val(randomNum1);
+        randomNum2 = Math.floor(Math.random()*(12-1)+1);
+        $("#choice2").val(randomNum2);
+        randomNum3 = Math.floor(Math.random()*(12-1)+1);
+        $("#choice3").val(randomNum3);
+        randomNum4 = Math.floor(Math.random()*(12-1)+1);
+        $("#choice4").val(randomNum4);
     };
 
     $("#choice1").on("click", function(){
